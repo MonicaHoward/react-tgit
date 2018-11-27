28f2435e2df2178efa0685d921973d5a977e0983
Monica Howard <jay.m.howard@gmail.com>
2018-11-26 21:32:43 -0600

    Added MainNav to layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import shows from './shows';
 
+import MainNav from './MainNav'
 import Show from './Show';
 
 class App extends Component {
   render() {
     return (
       <div>
+          <MainNav />
           <Show show={shows[1]}/>
       </div>
     );
---
+++ src/MainNav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class MainNav extends Component {
+    render(){
+        return(
+            <div>
+                <p>logo</p>
+                <p>Shows</p>
+                <p>Live</p>
+                <p>Schedule</p>
+                <p>Account</p>
+                <p>More</p>
+            </div>
+        )
+    }
+}
+
+export default MainNav;
